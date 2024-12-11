dc60bbb1ceb17002a11adc674ef6817eaa6eeaa2
mdshakil04 <m.shakil04@yahoo.com>
2024-12-11 21:09:57 +0600

    Nav and footer components added

--- src/layouts/MainLayouts.jsx
+++ src/layouts/MainLayouts.jsx
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom"
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
 
 const MainLayouts = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className=" ">
+      <div>
+        <Navbar/>
+      </div>
       <Outlet></Outlet>
-      <h1>Footer</h1>
+      <div>
+        <Footer/>
+      </div>
     </div>
   )
 }
